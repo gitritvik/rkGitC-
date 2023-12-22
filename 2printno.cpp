79d7c7bb6c7e631d103efd28e3e511dc020eaf7f
gitritvik <ritvikrai789@gmail.com>
2023-12-22 10:46:12 +0530

    In this program we are going to take an integer from user as input and show it to the user

---
+++ 2printno.cpp
@@ -0,0 +1,14 @@
+// In this program we are going to print an integer no. entered by user
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+
+    cout<<"Enter a no.- ";
+    cin>>a;
+    cout<<"No. Entered by the user - "<<a;
+    return 0;
+}
