0c413d25c05ee031b402d97f21a8418b676eb98a
gitritvik <ritvikrai789@gmail.com>
2023-12-22 13:42:05 +0530

    Ascci val of any charetecte entred by the user

---
+++ 7ASCIIval.cpp
@@ -0,0 +1,15 @@
+// Write a Program to Find ASCII Value of a Character
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    char d;
+    cout<<" Enter the charcter of you want to find the ASCII value- "<<endl;
+    cin>>d;
+    cout<<"ASCII value to the chareter Entered by you is- "<<int(d); // this is the logic to convert char to ascii.
+
+
+
+    return 0;
+}
