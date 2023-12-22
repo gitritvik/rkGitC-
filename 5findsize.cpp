292b205f44fd6a58371347e1b9df44603dc1970b
gitritvik <ritvikrai789@gmail.com>
2023-12-22 11:57:55 +0530

    this is the program to find the size of any datatype in that perticular machine

---
+++ 5findsize.cpp
@@ -0,0 +1,20 @@
+// Write a Program to Find Size of int, float, double and char
+// // in your computer
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    // size of diffrent datatypes using siezeof(datatype_name)funcion;
+    
+    cout<<"Size of the int is- "<<sizeof(int)<<endl;
+    cout<<"Size of the float is- "<<sizeof(float)<<endl;
+    cout<<"Size of the double is- "<<sizeof(double)<<endl;
+    cout<<"Size of the char is- "<<sizeof(char)<<endl;
+
+
+    return 0;
+}
+   
