049c7549631b9117968904255966a89ae34f41ea
gitritvik <ritvikrai789@gmail.com>
2023-12-22 14:01:46 +0530

    This program tell that a given no. is even or odd

---
+++ 9oddeven.cpp
@@ -0,0 +1,28 @@
+// Find that no. is odd or even;
+
+
+#include <iostream>
+using namespace std;
+
+//function to find no. is even or odd
+
+void oddeve(int a){
+    
+    if(a%2 == 0)
+    cout<<"Entered no. is even";
+    else 
+    cout<<"Entered no. is Odd";
+
+
+    
+}
+
+
+int main(){
+
+    int a;
+    cout<<"Enter any no. find out weather it is even or odd- ";
+    cin>>a;
+    oddeve(a);
+    return 0;
+}
